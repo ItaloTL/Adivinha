ca1cae709d201ce46e3ea854d3cfb5b4cf34d2e1
ItaloTL <itltrancoso@gmail.com>
2025-03-14 21:51:56 -0300

    Tarefa concluída, Adivinha

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Atividade Adivinha
+
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("+---------------------+");
+Console.Write("|");
+Console.ResetColor();
+Console.Write(" O que é o que é...");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("  |");
+Console.WriteLine("+---------------------+");
+Console.ResetColor();
+Thread.Sleep(1200);
+Console.Write("É mais leve que uma pluma,");
+Thread.Sleep(1300);
+Console.WriteLine(" mas nem o homem mais forte do mundo pode segurá-la por mais de um minuto?");
+Thread.Sleep(1000);
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write(".");
+Thread.Sleep(1000);
+Console.Write(".");
+Thread.Sleep(1000);
+Console.WriteLine(".");
+Console.ResetColor();
+Thread.Sleep(3000);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("A respiração");
+Console.ResetColor();
